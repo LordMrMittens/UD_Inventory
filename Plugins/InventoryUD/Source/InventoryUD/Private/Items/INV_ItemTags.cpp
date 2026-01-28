f67095dc800c461253e80330884380bdac875791
Mad Mex <74403807+LordMrMittens@users.noreply.github.com>
2026-01-28 10:38:41 +0000

    Lesson 27 Complete

---
+++ Plugins/InventoryUD/Source/InventoryUD/Private/Items/INV_ItemTags.cpp
@@ -0,0 +1,47 @@
+#include "Items\INV_ItemTags.h"
+
+namespace GameItems
+{
+	namespace Equipment
+	{
+		namespace Weapons
+		{
+			UE_DEFINE_GAMEPLAY_TAG_COMMENT(Axe, "GameItems.Equipment.Weapons.Axe", "Axe")
+			UE_DEFINE_GAMEPLAY_TAG_COMMENT(Sword, "GameItems.Equipment.Weapons.Sword","Sword")
+		}
+		namespace Cloaks
+		{
+			UE_DEFINE_GAMEPLAY_TAG_COMMENT(BlueCloak, "GameItems.Equipment.Cloaks.BlueCloak", "BlueCloak")
+		}
+		namespace Masks
+		{
+			UE_DEFINE_GAMEPLAY_TAG_COMMENT(SteelMask, "GameItems.Equipment.Masks.SteelMask", "SteelMask")
+		}
+
+	}
+	namespace Consumables
+	{
+		namespace Potions
+		{
+			namespace Red
+			{
+				UE_DEFINE_GAMEPLAY_TAG_COMMENT(Small, "GameItems.Potions.Red.Small", "Small")
+				UE_DEFINE_GAMEPLAY_TAG_COMMENT(Large, "GameItems.Potions.Red.Large", "Large")
+			}
+			namespace Blue
+			{
+				UE_DEFINE_GAMEPLAY_TAG_COMMENT(Small, "GameItems.Potions.Blue.Small", "Small")
+				UE_DEFINE_GAMEPLAY_TAG_COMMENT(Large, "GameItems.Potions.Blue.Large", "Large")
+			}
+		}
+
+	}
+	namespace Craftables
+	{
+		UE_DEFINE_GAMEPLAY_TAG_COMMENT(FireFruit, "GameItems.Craftables.FireFruit", "FireFruit")
+		UE_DEFINE_GAMEPLAY_TAG_COMMENT(LuminDaisy, "GameItems.Craftables.LuminDaisy", "LuminDaisy")
+		UE_DEFINE_GAMEPLAY_TAG_COMMENT(PetalBlossom, "GameItems.Craftables.PetalBlossom", "PetalBlossom")
+
+
+	}
+}
--- Plugins/InventoryUD/Source/InventoryUD/Public/Items/Manifest/INV_ItemManifest.h
+++ Plugins/InventoryUD/Source/InventoryUD/Public/Items/Manifest/INV_ItemManifest.h
@@ -4,7 +4,9 @@
 
 #include "CoreMinimal.h"
 #include "Types/INV_GridTypes.h"
+#include "Items/INV_ItemTags.h"
 #include "StructUtils/InstancedStruct.h"
+#include "GameplayTagContainer.h"
 #include "INV_ItemManifest.generated.h"
 
 
@@ -21,11 +23,14 @@ private:
 
 
 	UPROPERTY(EditAnywhere, Category = "Inventory")
-	EINV_ItemCategory ItemCategory{EINV_ItemCategory::None};
+	EINV_ItemCategory ItemCategory{ EINV_ItemCategory::None };
+	UPROPERTY(EditAnywhere, Category = "Inventory")
+	FGameplayTag ItemType;
 
 public:
 
 	UINV_InventoryItem* Manifest(UObject* NewOuter);
 
 	FORCEINLINE EINV_ItemCategory GetItemCategory() const { return ItemCategory; }
+	FORCEINLINE FGameplayTag GetItemType() const {return ItemType;}
 };
