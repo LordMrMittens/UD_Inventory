875354bad1b512615d78c6a57bca4dd61a29e80a
Mad Mex <74403807+LordMrMittens@users.noreply.github.com>
2026-02-10 14:38:18 +0000

    Lesson 86 Complete

--- Plugins/InventoryUD/Source/InventoryUD/Private/Widgets/Inventory/Spatial/INV_InventoryGrid.cpp
+++ Plugins/InventoryUD/Source/InventoryUD/Private/Widgets/Inventory/Spatial/INV_InventoryGrid.cpp
@@ -612,7 +612,17 @@ void UINV_InventoryGrid::ConstructGrid()
 
 void UINV_InventoryGrid::OnGridSlotClicked(int32 GridIndex, const FPointerEvent& MouseEvent)
 {
-
+	if (!IsValid(HoverItem)) return;
+	if (!GridSlots.IsValidIndex(ItemDropIndex)) return;
+	if (CurrentQueryResult.ValidItem.IsValid() && GridSlots.IsValidIndex(CurrentQueryResult.UpperLeftIndex)) 
+	{
+		OnSlottedItemClicked(CurrentQueryResult.UpperLeftIndex, MouseEvent);
+		return;
+	}
+	auto GridSlot = GridSlots[ItemDropIndex];
+	if (!GridSlot->GetInventoryItem().IsValid()) {
+		// put item down at index
+	}
 }
 
 void UINV_InventoryGrid::OnGridSlotHovered(int32 GridIndex, const FPointerEvent& MouseEvent)
